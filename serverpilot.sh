acf6a8b42a6d02baab2b9f18a4b89338ff791b1c
Aphichat Panjamanee <panjamanee@gmail.com>
2019-05-12 09:14:31 +0200

    Update

--- serverpilot.sh
+++ serverpilot.sh
@@ -4,6 +4,7 @@
 # It generate and add the bare repository and add the post-receive hook file to the bare repository
 
 REPOSITORY_PATH="/srv/users/$(whoami)/apps/repositories"
+POST_RECEIVE_PATH="$REPOSITORY_PATH/$2.git/hooks"
 
 if [ $1 = "-repository" ] && [ $3 = "-branch" ]; then
   if [ ! -d "$REPOSITORY_PATH/$2.git" ]; then
@@ -11,11 +12,11 @@ if [ $1 = "-repository" ] && [ $3 = "-branch" ]; then
     # initiate --bare repo
     git init --bare $REPOSITORY_PATH/$2.git
     # copy the post-receive file into the repo
-    cp ./post-receive/serverpilot $REPOSITORY_PATH/$2.git/hooks/post-receive
+    cp ./post-receive/serverpilot $POST_RECEIVE_PATH/post-receive
     # update the variables of post-receive script
-    sed -i -e "s/<branch>/$4/g" -e "s/<repository>/$2/g" -e "s/<whoami>/$(whoami)/g" $REPOSITORY_PATH/$2.git/hooks/post-receive
+    sed -i -e "s/<branch>/$4/g" -e "s/<repository>/$2/g" -e "s/<whoami>/$(whoami)/g" $POST_RECEIVE_PATH/post-receive
     # make sure <user> can excecute the post-receive file
-    chmod +x $REPOSITORY_PATH/$2.git/hooks/post-receive
+    chmod +x $POST_RECEIVE_PATH/post-receive
   else
     echo "Error, the --bare repository allready exist."
   fi
