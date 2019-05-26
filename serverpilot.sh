af1d9b7cfd615c050e4d834bd39e804faf5013e1
Aphichat Panjamanee <panjamanee@gmail.com>
2019-05-26 23:04:30 +0200

    WIP

--- serverpilot.sh
+++ serverpilot.sh
@@ -3,20 +3,21 @@
 # This script initialise the auto deployment process
 # It generate and add the bare repository and add the post-receive hook file to the bare repository
 
-POST_RECEIVE_PATH="../$2.git/hooks"
+POST_RECEIVE_PATH="$2.git/hooks"
 
 echo "----"
 echo "WHOAMI: $(whoami)"
+echo "REPOSITORY: $8"
 echo "BRANCH: $4"
 echo "GIT_DIR: $2"
 echo "WORK_TREE: $6"
 echo "POST_RECEIVE_PATH: $POST_RECEIVE_PATH"
 echo "----"
 
-if [ $1 = "-git_dir" ] && [ $3 = "-branch" ] && [ $5 = "-path" ]; then
-  if [ ! -d "../$2.git" ]; then
-    echo "Initiate --bare repo"
-    git init --bare ../$2.git
+if [ $1 = "-git_dir" ] && [ $3 = "-branch" ] && [ $5 = "-path" ] && [ $7 = "-repository" ]; then
+  if [ ! -d "$2.git" ]; then
+    echo "Initiate --bare repo $2.git"
+    git init --bare $8.git
     echo "Copy the post-receive file into the repo"
     cp ./post-receive/serverpilot $POST_RECEIVE_PATH/post-receive
     echo "Substitude the variables of post-receive script"
