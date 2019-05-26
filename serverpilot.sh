31bf55ff60e18843153943a7e2cc87c48fb8ba47
Aphichat Panjamanee <panjamanee@gmail.com>
2019-05-26 21:51:14 +0200

    Update

--- serverpilot.sh
+++ serverpilot.sh
@@ -3,18 +3,16 @@
 # This script initialise the auto deployment process
 # It generate and add the bare repository and add the post-receive hook file to the bare repository
 
-REPOSITORY_PATH=$6
-POST_RECEIVE_PATH="$REPOSITORY_PATH/$2.git/hooks"
+POST_RECEIVE_PATH="../$2.git/hooks"
 
 if [ $1 = "-repository" ] && [ $3 = "-branch" ] && [ $5 = "-path" ]; then
-  if [ ! -d "$REPOSITORY_PATH/$2.git" ]; then
-    echo "REPOSITORY_PATH = $REPOSITORY_PATH"
+  if [ ! -d "../$2.git" ]; then
     # initiate --bare repo
-    git init --bare $REPOSITORY_PATH/$2.git
+    git init --bare ../$2.git
     # copy the post-receive file into the repo
     cp ./post-receive/serverpilot $POST_RECEIVE_PATH/post-receive
-    # update the variables of post-receive script
-    sed -i -e "s/<branch>/$4/g" -e "s/<repository>/$2/g" -e "s/<whoami>/$(whoami)/g" $POST_RECEIVE_PATH/post-receive
+    # substitude the variables of post-receive script
+    sed -i -e "s/<branch>/$4/g" -e "s/<repository>/$2/g" -e "s/<whoami>/$(whoami)/g" -e "s/<work_tree>/$6/g" $POST_RECEIVE_PATH/post-receive
     # make sure <user> can excecute the post-receive file
     chmod +x $POST_RECEIVE_PATH/post-receive
   else
