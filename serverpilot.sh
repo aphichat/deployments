d6861ffb5dbc31c16c025ac14ae2bd73ef4fac75
Aphichat Panjamanee <panjamanee@gmail.com>
2019-05-26 22:16:44 +0200

    Fixes

--- serverpilot.sh
+++ serverpilot.sh
@@ -5,15 +5,23 @@
 
 POST_RECEIVE_PATH="../$2.git/hooks"
 
+echo "----"
+echo "WHOAMI: $(whoami)"
+echo "REPOSITORY: $2"
+echo "BRANCH: $4"
+echo "WORK_TREE: $6"
+echo "POST_RECEIVE_PATH: $POST_RECEIVE_PATH"
+echo "----"
+
 if [ $1 = "-repository" ] && [ $3 = "-branch" ] && [ $5 = "-path" ]; then
   if [ ! -d "../$2.git" ]; then
-    # initiate --bare repo
+    echo "Initiate --bare repo"
     git init --bare ../$2.git
-    # copy the post-receive file into the repo
+    echo "Copy the post-receive file into the repo"
     cp ./post-receive/serverpilot $POST_RECEIVE_PATH/post-receive
-    # substitude the variables of post-receive script
+    echo "Substitude the variables of post-receive script"
     sed -i -e "s/<branch>/$4/g" -e "s/<repository>/$2/g" -e "s/<whoami>/$(whoami)/g" -e "s/<work_tree>/$6/g" $POST_RECEIVE_PATH/post-receive
-    # make sure <user> can excecute the post-receive file
+    echo "Make sure <user> can excecute the post-receive file"
     chmod +x $POST_RECEIVE_PATH/post-receive
   else
     echo "Error, the --bare repository allready exist."
