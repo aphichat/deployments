0ee7509979c024b4a0aa842c040e2cd15b21c38c
Aphichat Panjamanee <panjamanee@gmail.com>
2019-05-26 22:53:18 +0200

    WIP

--- serverpilot.sh
+++ serverpilot.sh
@@ -7,20 +7,20 @@ POST_RECEIVE_PATH="../$2.git/hooks"
 
 echo "----"
 echo "WHOAMI: $(whoami)"
-echo "REPOSITORY: $2"
 echo "BRANCH: $4"
+echo "GIT_DIR: $2"
 echo "WORK_TREE: $6"
 echo "POST_RECEIVE_PATH: $POST_RECEIVE_PATH"
 echo "----"
 
-if [ $1 = "-repository" ] && [ $3 = "-branch" ] && [ $5 = "-path" ]; then
+if [ $1 = "-git_dir" ] && [ $3 = "-branch" ] && [ $5 = "-path" ]; then
   if [ ! -d "../$2.git" ]; then
     echo "Initiate --bare repo"
     git init --bare ../$2.git
     echo "Copy the post-receive file into the repo"
     cp ./post-receive/serverpilot $POST_RECEIVE_PATH/post-receive
     echo "Substitude the variables of post-receive script"
-    sed -i -e "s/<branch>/$4/g" -e "s/<repository>/$2/g" -e "s/<whoami>/$(whoami)/g" -e "s/<work_tree>/$6/g" $POST_RECEIVE_PATH/post-receive
+    sed -i -e "s/<branch>/$4/g" -e "s/<git_dir>/$2/g" -e "s/<whoami>/$(whoami)/g" -e "s/<work_tree>/$6/g" $POST_RECEIVE_PATH/post-receive
     echo "Make sure <user> can excecute the post-receive file"
     chmod +x $POST_RECEIVE_PATH/post-receive
   else
